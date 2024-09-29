796970cac1bf8f3f7ed60da10d2ee310cc3c6daa
Ben Stolovitz <ben@stolovitz.com>
2024-09-29 16:24:28 +0300

    dispatcher queue?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+use windows::{
+    core::Result,
+    Win32::UI::WindowsAndMessaging::{DispatchMessageW, GetMessageW, TranslateMessage, MSG}
+};
+
+
+
+fn run() -> Result<()> {
     println!("Hello, world!");
+
+    let mut message = MSG::default();
+    unsafe {
+        while GetMessageW(&mut message, None, 0, 0).into() {
+            _ = TranslateMessage(&message);
+            DispatchMessageW(&message);
+        }
+    }
+    Ok(())
+}
+
+fn main() {
+    let result = run();
+
+    // We do this for nicer HRESULT printing when errors occur.
+    if let Err(error) = result {
+        error.code().unwrap();
+    }
 }
