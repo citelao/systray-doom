399753b23b6fdf7b904b4512d716f98a294f1e20
Ben Stolovitz <ben@stolovitz.com>
2024-10-23 23:03:55 -0400

    yay

---
+++ systray_doom/PInvokeDoom.cs
@@ -0,0 +1,34 @@
+using System.Runtime.InteropServices;
+
+public static partial class PInvokeDoom
+{
+    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+    public static partial Int32 rust_function();
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct CKeyData
+    {
+        public bool pressed;
+        public byte key;
+    };
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public unsafe delegate void DrawFrameDelegate(UInt32* frame, nint xres, nint yres);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public unsafe delegate CKeyData* KeyCallbackDelegate();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public unsafe delegate void SetWindowTitleDelegate(byte* title, nint size);
+
+    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+    public static partial IntPtr create_game(
+        DrawFrameDelegate draw_frame,
+        KeyCallbackDelegate key_callback,
+        SetWindowTitleDelegate set_window_title
+    );
+
+    // pub extern "C" fn start_game(game: *mut PublicGame) {
+    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", SetLastError = true)]
+    public static partial void start_game(IntPtr game);
+}
--- systray_doom/Program.cs
+++ systray_doom/Program.cs
@@ -2,14 +2,14 @@
 
 Console.WriteLine("Hello, World!");
 
-var i = PInvoke.rust_function();
+var i = PInvokeDoom.rust_function();
 Console.WriteLine(i);
 
 static unsafe void DrawFrame(UInt32* frame, nint xres, nint yres)
 {
     // Console.WriteLine("DrawFrame");
 }
-static unsafe PInvoke.CKeyData* KeyCallback()
+static unsafe PInvokeDoom.CKeyData* KeyCallback()
 {
     // Console.WriteLine("KeyCallback");
     return null;
@@ -21,44 +21,11 @@ static unsafe void SetWindowTitle(byte* title, nint size)
 
 unsafe
 {
-    var game = PInvoke.create_game(
+    var game = PInvokeDoom.create_game(
         DrawFrame,
         KeyCallback,
         SetWindowTitle
     );
 
-    PInvoke.start_game(game);
-}
-
-public static partial class PInvoke
-{
-    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
-    public static partial Int32 rust_function();
-
-    [StructLayout(LayoutKind.Sequential)]
-    public unsafe struct CKeyData
-    {
-        public bool pressed;
-        public byte key;
-    };
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public unsafe delegate void DrawFrameDelegate(UInt32* frame, nint xres, nint yres);
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public unsafe delegate CKeyData* KeyCallbackDelegate();
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public unsafe delegate void SetWindowTitleDelegate(byte* title, nint size);
-
-    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
-    public static partial IntPtr create_game(
-        DrawFrameDelegate draw_frame,
-        KeyCallbackDelegate key_callback,
-        SetWindowTitleDelegate set_window_title
-    );
-
-    // pub extern "C" fn start_game(game: *mut PublicGame) {
-    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", SetLastError = true)]
-    public static partial void start_game(IntPtr game);
+    PInvokeDoom.start_game(game);
 }
