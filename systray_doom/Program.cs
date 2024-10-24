4eef44f0d4225891b2fbc97dded49f5399ae2caa
Ben Stolovitz <ben@stolovitz.com>
2024-10-23 21:48:00 -0400

    dotnet...

---
+++ systray_doom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public static partial class PInvoke
+{
+    [LibraryImport("user32.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+    public static partial int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+}
