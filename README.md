42a737d109fefeac5493a05ac216e02bbb754e95
Ben Stolovitz <ben@stolovitz.com>
2024-10-23 22:41:14 -0400

    yay

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ git submodule init
 ## Usage
 
 TODO
+
+## TODO
+
+* [ ] Upgrade to a newer commit of Doomgeneric (specifically, one after the [sound commit](https://github.com/ozkl/doomgeneric/commit/d0946b46cf617467f014a25e264fd952698a13f9))
--- systray_doom/Program.cs
+++ systray_doom/Program.cs
@@ -8,8 +8,51 @@ Console.WriteLine("Hello, World!");
 var i = PInvoke.rust_function();
 Console.WriteLine(i);
 
+static void DrawFrame(IntPtr frame, nint xres, nint yres)
+{
+    Console.WriteLine("DrawFrame");
+}
+static PInvoke.CKeyData? KeyCallback()
+{
+    Console.WriteLine("KeyCallback");
+    return null;
+}
+static void SetWindowTitle(IntPtr title, nint size)
+{
+    Console.WriteLine("SetWindowTitle");
+}
+
+PInvoke.create_game(
+    DrawFrame,
+    KeyCallback,
+    SetWindowTitle
+);
+
 public static partial class PInvoke
 {
     [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
     public static partial Int32 rust_function();
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct CKeyData
+    {
+        public bool pressed;
+        public byte key;
+    };
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void DrawFrameDelegate(IntPtr frame, nint xres, nint yres);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate CKeyData? KeyCallbackDelegate();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void SetWindowTitleDelegate(IntPtr title, nint size);
+
+    [LibraryImport("../rust_bindings/target/debug/systray_doom_bindings.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+    public static partial IntPtr create_game(
+        DrawFrameDelegate draw_frame,
+        KeyCallbackDelegate key_callback,
+        SetWindowTitleDelegate set_window_title
+    );
 }
