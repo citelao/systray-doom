13d49382153ccf8e7e09b0fc9a8688ef5253e615
Ben Stolovitz <ben@stolovitz.com>
2025-08-13 00:17:35 -0400

    unit tests?

---
+++ Systray.UnitTests/TrayIconUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Systray;
+
+namespace Systray.UnitTests;
+
+public class TrayIconUnitTests
+{
+    public static readonly Guid s_guid = new("0e1e23de-db34-4dac-b0ca-49424eac3bbd");
+
+    [Fact]
+    public void TestBasicPropertyValidation()
+    {
+        var icon = new Systray.TrayIcon(guid: s_guid, ownerHwnd: Systray.NoReleaseHwnd.Null, shouldHandleMessages: false, callbackMessage: 0x0400 + 1);
+    }
+}
