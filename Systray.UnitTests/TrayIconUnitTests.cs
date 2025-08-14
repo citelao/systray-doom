73388de0f0261003b1ec8ba66cbe2e26ec0d0040
Ben Stolovitz <ben@stolovitz.com>
2025-08-13 23:05:29 -0400

    test tooltip

--- Systray.UnitTests/TrayIconUnitTests.cs
+++ Systray.UnitTests/TrayIconUnitTests.cs
@@ -1,10 +1,13 @@
 ﻿using Systray.NativeTypes;
+using Windows.Win32;
+using Windows.Win32.UI.Shell;
 
 namespace Systray.UnitTests;
 
 public class TrayIconUnitTests : IDisposable
 {
     public static readonly Guid s_guid = new("0e1e23de-db34-4dac-b0ca-49424eac3bbd");
+    public static readonly NoReleaseHwnd s_fakeHwnd = new(0x12345678);
 
     public void Dispose()
     {
@@ -14,7 +17,7 @@ public class TrayIconUnitTests : IDisposable
     [Fact]
     public void TestBasicPropertyValidation()
     {
-        TrayIcon.Shell_NotifyIconFn = (Windows.Win32.UI.Shell.NOTIFY_ICON_MESSAGE dwMessage, in NOTIFYICONDATAW lpData) =>
+        TrayIcon.Shell_NotifyIconFn = (NOTIFY_ICON_MESSAGE dwMessage, in NOTIFYICONDATAW lpData) =>
         {
             // We don't actually want to do anything here.
             return true;
@@ -23,12 +26,85 @@ public class TrayIconUnitTests : IDisposable
         uint callbackMessage = 0x0400 + 1;
         var icon = new TrayIcon(
             guid: s_guid,
-            ownerHwnd: NoReleaseHwnd.Null,
+            ownerHwnd: s_fakeHwnd,
             shouldHandleMessages: false,
             callbackMessage: callbackMessage);
 
         Assert.Equal(s_guid, icon.Guid);
-        Assert.Equal(NoReleaseHwnd.Null, icon.OwnerHwnd);
+        Assert.Equal(s_fakeHwnd, icon.OwnerHwnd);
         Assert.Equal(callbackMessage, icon.CallbackMessage);
+        Assert.Equal("", icon.Tooltip);
+        Assert.NotEqual(NoReleaseHicon.Null, icon.Icon);
+    }
+
+    [Fact]
+    public void TestRegistrationFunctions()
+    {
+        var messages = new List<(NOTIFY_ICON_MESSAGE Message, NOTIFYICONDATAW Data)>();
+        TrayIcon.Shell_NotifyIconFn = (NOTIFY_ICON_MESSAGE dwMessage, in NOTIFYICONDATAW lpData) =>
+        {
+            messages.Add((dwMessage, lpData));
+            return true;
+        };
+
+        var icon = new TrayIcon(
+            guid: s_guid,
+            ownerHwnd: s_fakeHwnd,
+            shouldHandleMessages: false);
+
+        Assert.Equal(2, messages.Count);
+
+        // First, we add the icon.
+        Assert.Equal(NOTIFY_ICON_MESSAGE.NIM_ADD, messages[0].Message);
+        Assert.Equal(s_fakeHwnd.AsHWND(), messages[0].Data.hWnd);
+        Assert.Equal(0u, messages[0].Data.uID);
+        Assert.Equal(NOTIFY_ICON_DATA_FLAGS.NIF_GUID | NOTIFY_ICON_DATA_FLAGS.NIF_ICON | NOTIFY_ICON_DATA_FLAGS.NIF_TIP | NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP, messages[0].Data.uFlags);
+        Assert.Equal(0u, messages[0].Data.uCallbackMessage);
+        Assert.NotEqual(NoReleaseHicon.Null.AsHICON(), messages[0].Data.hIcon);
+        Assert.Equal("", messages[0].Data.Tip.ToString());
+        Assert.Equal(default, messages[0].Data.dwState);
+        Assert.Equal(default, messages[0].Data.dwStateMask);
+        Assert.Equal("", messages[0].Data.Info);
+        Assert.Equal(PInvokeSystray.NOTIFYICON_VERSION_4, messages[0].Data.Anonymous.uVersion);
+        Assert.Equal("", messages[0].Data.InfoTitle);
+        Assert.Equal(NOTIFY_ICON_INFOTIP_FLAGS.NIIF_NONE, messages[0].Data.dwInfoFlags);
+        Assert.Equal(s_guid, messages[0].Data.guidItem);
+        Assert.Equal(NoReleaseHicon.Null.AsHICON(), messages[0].Data.hBalloonIcon);
+
+        // Then, we set the version.
+        Assert.Equal(NOTIFY_ICON_MESSAGE.NIM_SETVERSION, messages[1].Message);
+        Assert.Equal(NOTIFY_ICON_DATA_FLAGS.NIF_GUID | NOTIFY_ICON_DATA_FLAGS.NIF_ICON | NOTIFY_ICON_DATA_FLAGS.NIF_TIP | NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP, messages[1].Data.uFlags);
+        Assert.Equal(s_guid, messages[1].Data.guidItem);
+        Assert.Equal(PInvokeSystray.NOTIFYICON_VERSION_4, messages[1].Data.Anonymous.uVersion);
+    }
+
+    [Fact]
+    public void TestSetTooltip()
+    {
+        var messages = new List<(NOTIFY_ICON_MESSAGE Message, NOTIFYICONDATAW Data)>();
+        TrayIcon.Shell_NotifyIconFn = (NOTIFY_ICON_MESSAGE dwMessage, in NOTIFYICONDATAW lpData) =>
+        {
+            messages.Add((dwMessage, lpData));
+            return true;
+        };
+
+        var icon = new TrayIcon(
+            guid: s_guid,
+            ownerHwnd: s_fakeHwnd,
+            shouldHandleMessages: false);
+
+        Assert.Equal(2, messages.Count);
+
+        // Change the tooltip.
+        messages.Clear();
+        icon.Tooltip = "New Tooltip";
+
+        Assert.Single(messages);
+        Assert.Equal(NOTIFY_ICON_MESSAGE.NIM_MODIFY, messages[0].Message);
+        Assert.Equal(s_guid, messages[0].Data.guidItem);
+        Assert.Equal(NOTIFY_ICON_DATA_FLAGS.NIF_TIP | NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP | NOTIFY_ICON_DATA_FLAGS.NIF_GUID, messages[0].Data.uFlags);
+        Assert.Equal("New Tooltip", messages[0].Data.Tip.ToString());
+
+        Assert.Equal("New Tooltip", icon.Tooltip);
     }
 }
