2732ac3efcf2cf48abb1e674abcc4d1bd6ccd3a2
Ben Stolovitz <ben@stolovitz.com>
2025-10-24 14:39:34 -0400

    window begins

--- Systray.Samples/MessageOnlyWindow.cs
+++ Systray.Samples/MessageOnlyWindow.cs
@@ -5,12 +5,31 @@ using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
 
-public class MessageOnlyWindow : IDisposable
+internal class MessageOnlyWindow : IDisposable
 {
+    internal delegate LRESULT? WndProcDelegate(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam);
+
+    private static readonly Dictionary<int, WeakReference<MessageOnlyWindow>> s_handlers = [];
+    private static int s_nextId = 1;
+    public struct Data
+    {
+        public int ID;
+    }
+
     internal readonly HWND Hwnd;
 
-    unsafe public MessageOnlyWindow(string name)
+    private Data _data;
+    private readonly WndProcDelegate _wndProc;
+
+    unsafe public MessageOnlyWindow(string name, WndProcDelegate wndProc)
     {
+        _wndProc = wndProc;
+        _data = new Data
+        {
+            ID = s_nextId++,
+        };
+        s_handlers[_data.ID] = new WeakReference<MessageOnlyWindow>(this);
+
         fixed (char* fixedName = name)
         {
             var wndClass = new WNDCLASSEXW
@@ -49,37 +68,37 @@ public class MessageOnlyWindow : IDisposable
     [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
     private static LRESULT StaticWndProc(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
     {
-        // switch (msg)
-        // {
-        //     case PInvoke.WM_NCCREATE:
-        //         unsafe
-        //         {
-        //             var createStruct = (CREATESTRUCTW*)lParam.Value;
-        //             var data = (Data*)createStruct->lpCreateParams;
-        //             PInvoke.SetWindowLongPtr(hwnd, WINDOW_LONG_PTR_INDEX.GWLP_USERDATA, (nint)data);
-        //         }
-        //         return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
+        switch (msg)
+        {
+            case PInvoke.WM_NCCREATE:
+                unsafe
+                {
+                    var createStruct = (CREATESTRUCTW*)lParam.Value;
+                    var data = (Data*)createStruct->lpCreateParams;
+                    Systray.NativeTypes.PInvokeCore.SetWindowLongPtr(new Systray.NativeTypes.NoReleaseHwnd(hwnd.Value), (int)WINDOW_LONG_PTR_INDEX.GWLP_USERDATA, (nint)data);
+                }
+                return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
 
-        //     default:
-        //         unsafe
-        //         {
-        //             var data = (Data*)PInvoke.GetWindowLongPtr(hwnd, WINDOW_LONG_PTR_INDEX.GWLP_USERDATA);
-        //             if (data != null)
-        //             {
-        //                 var id = data->ID;
-        //                 if (s_handlers.TryGetValue(id, out var weakRef) && weakRef.TryGetTarget(out WindowMessageHandler? that))
-        //                 {
-        //                     var result = that?._wndProc(hwnd, msg, wParam, lParam);
-        //                     if (result != null)
-        //                     {
-        //                         return result.Value;
-        //                     }
-        //                 }
-        //             }
-        //         }
+            default:
+                unsafe
+                {
+                    var data = (Data*)Systray.NativeTypes.PInvokeCore.GetWindowLongPtr(new Systray.NativeTypes.NoReleaseHwnd(hwnd.Value), (int)WINDOW_LONG_PTR_INDEX.GWLP_USERDATA);
+                    if (data != null)
+                    {
+                        var id = data->ID;
+                        if (s_handlers.TryGetValue(id, out var weakRef) && weakRef.TryGetTarget(out MessageOnlyWindow? that))
+                        {
+                            var result = that?._wndProc(hwnd, msg, wParam, lParam);
+                            if (result != null)
+                            {
+                                return result.Value;
+                            }
+                        }
+                    }
+                }
 
                 return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
-        // }
+        }
     }
 
     // TODO: message loop
--- Systray.Samples/Program.cs
+++ Systray.Samples/Program.cs
@@ -5,7 +5,22 @@ using Windows.Win32.UI.WindowsAndMessaging;
 
 Console.WriteLine("Hello, World!");
 
-using var messageWindow = new MessageOnlyWindow("Systray.Sample.Window");
+using var messageWindow = new MessageOnlyWindow("Systray.Sample.Window", (hwnd, msg, wParam, lParam) =>
+{
+    switch (msg)
+    {
+        case PInvoke.WM_CLOSE:
+            PInvoke.DestroyWindow(hwnd);
+            return new LRESULT(0);
+
+        case PInvoke.WM_DESTROY:
+            PInvoke.PostQuitMessage(0);
+            return new LRESULT(0);
+
+        default:
+            return null;
+    }
+});
 
 // Wait for enter key
 Console.WriteLine("Press Enter to exit...");
